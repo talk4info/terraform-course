debcc9150986b2547bf1b478a7dcdc8d624e779c
talk4info <81502381+talk4info@users.noreply.github.com>
2021-08-21 23:20:38 +0530

    Update instance.tf

--- jenkins-packer-demo/instance.tf
+++ jenkins-packer-demo/instance.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "jenkins-instance" {
 }
 
 resource "aws_ebs_volume" "jenkins-data" {
-  availability_zone = "eu-west-1a"
+  availability_zone = "eu-west-2a"
   size              = 20
   type              = "gp2"
   tags = {
