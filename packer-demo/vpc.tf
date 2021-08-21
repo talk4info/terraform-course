13fbee03b7f6b55992c275ee73694e794555554a
talk4info <81502381+talk4info@users.noreply.github.com>
2021-08-21 23:34:30 +0530

    Update vpc.tf

--- packer-demo/vpc.tf
+++ packer-demo/vpc.tf
@@ -1,6 +1,6 @@
 # Internet VPC
 resource "aws_vpc" "main" {
-  cidr_block           = "172.31.0.0/24"
+  cidr_block           = "172.31.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
@@ -15,7 +15,7 @@ resource "aws_subnet" "main-public-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "172.31.0.0/20"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-2a"
+  availability_zone       = "eu-west-2c"
 
   tags = {
     Name = "main-public-1"
@@ -26,7 +26,7 @@ resource "aws_subnet" "main-public-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "172.31.16.0/20"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-2b"
+  availability_zone       = "eu-west-2a"
 
   tags = {
     Name = "main-public-2"
@@ -37,7 +37,7 @@ resource "aws_subnet" "main-public-3" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "172.31.32.0/20"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-2c"
+  availability_zone       = "eu-west-2b"
 
   tags = {
     Name = "main-public-3"
@@ -48,7 +48,7 @@ resource "aws_subnet" "main-private-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "172.31.64.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-2a"
+  availability_zone       = "eu-west-2b"
 
   tags = {
     Name = "main-private-1"
@@ -59,7 +59,7 @@ resource "aws_subnet" "main-private-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "172.31.65.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-2b"
+  availability_zone       = "eu-west-2c"
 
   tags = {
     Name = "main-private-2"
@@ -70,7 +70,7 @@ resource "aws_subnet" "main-private-3" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "172.31.66.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-2c"
+  availability_zone       = "eu-west-2a"
 
   tags = {
     Name = "main-private-3"
